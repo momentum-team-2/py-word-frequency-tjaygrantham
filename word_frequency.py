d84cb90353d4ab2d49f6901e6b5e1aa343724d23
Tyler Grantham <tjaygrantham@gmail.com>
2020-06-17 14:46:49 -0400

    print word frequency

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,5 @@
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -7,7 +9,15 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    f = open(file, "r") # Open txt file
+    words = [word.lower() for word in f.read().strip(string.punctuation).split() if word not in STOP_WORDS] # Read txt file, strip punctuation, split into words excluding stop words
+    wordCounts = {word:words.count(word) for word in words} # Get a count for each word in the words list
+    sortedWords = sorted(wordCounts, key=wordCounts.get, reverse=True) # Sort words by their count
+    for i in range(10):
+        word = sortedWords[i]
+        count = wordCounts[word]
+        print(word.rjust(6), "|", count, "".join("*" for i in range(count))) # Print the 10 most common words
+    f.close() # Close txt file
 
 
 if __name__ == "__main__":
